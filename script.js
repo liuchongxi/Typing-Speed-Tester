f1f97540f0666454fc5037356906bafb92741088
Jacky <sla255@sfu.ca>
2018-07-29 18:53:51 -0700

    add all the event listener this the tester need.

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
+const originText = document.querySelector("#origin-text p").innerHTML;
 const testWrapper = document.querySelector(".test-wrapper");
 const testArea = document.querySelector("#test-area");
-const originText = document.querySelector("#origin-text p").innerHTML;
-const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
+const resetButton = document.querySelector("#reset");
 
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
+function timer() {
 
+}
 
 // Match the text entered with the provided text on the page:
-
+function matchText(){
+    let enteredText = testArea.value;
+    console.log(enteredText);
+}
 
 // Start the timer:
+function startTimer(){
 
+}
 
 // Reset everything:
+function reset(){
 
+}
 
 // Event listeners for keyboard input and the reset button:
+testArea.addEventListener("keypress", startTimer, false);
+testArea.addEventListener("keyup", matchText, false);
+resetButton.addEventListener("click", reset, false);
